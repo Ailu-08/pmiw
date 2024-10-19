d1f3d9b70c3201d9a9416df9f7187fe2129e28d1
Ailu_Avanzini <128924848+Ailu-08@users.noreply.github.com>
2024-10-19 01:05:23 -0300

    Add files via upload

---
+++ Pantallas.js
@@ -0,0 +1,17 @@
+// Funcion para Pantallas 1/2/3/4/5/6/7/8/11/14/15
+
+function StoryPant(screen) {
+    
+ image(images[screen], 0, 0, width, height); // Muestra la imagen (indice) según su pantalla
+     transparentRect();
+     showText(texts[screen]);
+      botones();
+  }
+  
+// Función para las pantallas finales y créditos
+function FinalScreen(screen, titleIndex, textIndex) {
+    image(images[screen], 0, 0, width, height); // Muestra la imagen según su pantalla
+    showTitleAndText(titles[titleIndex], texts[textIndex]); // Usar índices específicos
+    botonesFinales();
+    stopSound();
+}
