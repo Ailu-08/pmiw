35162371e772e54948c9d2a0ea3c865225c48328
Ailu_Avanzini <128924848+Ailu-08@users.noreply.github.com>
2024-11-16 23:56:14 -0300

    Update tpfinalparte2.js

--- tpfinalparte2/tpfinalparte2.js
+++ tpfinalparte2/tpfinalparte2.js
@@ -1,5 +1,5 @@
 // Alumnas: Clar Agustina Legajo: 93551/8, Ailen Avanzini Legajo: 94717/7  // Materia: pmiw // TP Final Parte 1 //Comision: 5// FDA // UNLP
-//Link Tutorial:
+//Link Tutorial: https://www.youtube.com/watch?v=HsGLQ7tebw4&ab_channel=AgustinaClar
 
 let juego;
 let dastan, traitor;
