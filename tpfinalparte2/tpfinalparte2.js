ecab30c50208eb90d480ce0446994e4dade7b284
Ailu_Avanzini <128924848+Ailu-08@users.noreply.github.com>
2024-12-07 12:50:49 -0300

    Update tpfinalparte2.js

--- tpfinalparte2/tpfinalparte2.js
+++ tpfinalparte2/tpfinalparte2.js
@@ -1,5 +1,5 @@
 // Alumnas: Clar Agustina Legajo: 93551/8, Ailen Avanzini Legajo: 94717/7  // Materia: pmiw // TP Final Parte 2 Rec//Comision: 5// FDA // UNLP
-//Link Tutorial:
+//Link Tutorial: https://www.youtube.com/watch?v=H0v4ZvO7EUY&ab_channel=AgustinaClar
 
 let juego;
 let song;
